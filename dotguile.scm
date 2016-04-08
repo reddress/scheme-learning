ec22bd5ea7a17972e5b373a9d0cb524adb4ab2c2
Heitor Chang <heitorchang@gmail.com>
2016-04-08 20:30:49 -0300

    add out to path

--- dotguile.scm
+++ dotguile.scm
@@ -7,8 +7,8 @@
 (define base-dir #f)
 
 (if (equal? (substring (vector-ref (uname) 0) 0 6) "CYGWIN")
-    (set! base-dir "/cygdrive/c/Users/Heitor/Desktop/emacs-24.3/bin/scheme-learning/")
-    (set! base-dir "/home/heitor/scheme-learning/"))
+    (set! base-dir "/cygdrive/c/Users/Heitor/Desktop/emacs-24.3/bin/scheme-learning/out/")
+    (set! base-dir "/home/heitor/scheme-learning/out/"))
 
 (define (base-file filename)
         (string-append base-dir filename))
